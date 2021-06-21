90dc7bbb53bee515e64bb2950cc76d0889e5b32f
ruchikashaw <ruchikashaw069@gmail.com>
2021-06-21 14:43:50 +0530

    minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import "firebase/storage";
 
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyChotuPPRlTww80Y5TCiaTbYGN5aW0Cv-k",
-    authDomain: "note-app-412a3.firebaseapp.com",
-    projectId: "note-app-412a3",
-    storageBucket: "note-app-412a3.appspot.com",
-    messagingSenderId: "1076041763531",
-    appId: "1:1076041763531:web:a2b676f154ca68d3bba94e"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 const db = app.firestore();
