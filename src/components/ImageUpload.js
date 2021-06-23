491900e1d6d6fbc037ef7cf1ae1a3d0cd58ad8f8
ruchikashaw <ruchikashaw069@gmail.com>
2021-06-23 12:12:14 +0530

    Copy to clipboard functionality added

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -2,18 +2,21 @@ import React, {useState, useEffect} from "react";
 import {storage} from "../firebase";
 import {CopyToClipboard} from "react-copy-to-clipboard";
 import { MdContentCopy } from "react-icons/md";
+import {IconContext} from "react-icons";
 
 
 
 const ImageUpload = () => {
     const [image,setImage]= useState(null);
-    const [url,setUrl]= useState(
-        localStorage.url ? JSON.parse(localStorage.url): []
+    const [url,setUrl]= useState(""
+        // localStorage.url ? JSON.parse(localStorage.url): []
     );
-    useEffect(() => {
-      localStorage.setItem("url",JSON.stringify(url));
-    },[url]);
+    // useEffect(() => {
+    //   localStorage.setItem("url",JSON.stringify(url));
+    // },[url]);
     
+    // JSON.stringify({url});
+
 
     const handleChange = e => {
         if (e.target.files[0]){
@@ -42,19 +45,20 @@ const ImageUpload = () => {
     }
     
     console.log("image:",image);
-    // const[isCopied,setIsCopied]=useState(false)
+    const[isCopied,setIsCopied]=useState(false)
 
     // const codeSnippet =  
-    //    "![]({url})";
+    //    ![]({url});
     
-        
+    //    JSON.stringify({url});  
 
-    // const onCopyText = () =>{
-        // setIsCopied(true);
-        // setTimeout(()=>{
-            // setIsCopied(false);
-        // },1000);
-    // }
+    const onCopyText = () =>{
+        setIsCopied(true);
+        setTimeout(()=>{
+            setIsCopied(false);
+        },1000);
+    }
+    console.log('Copied');
     return(
         <div className="filehandle">
             <br/>
@@ -64,10 +68,13 @@ const ImageUpload = () => {
              <button onClick={handleUpload}>Upload</button>
              <br/>
              
-             ![]({url})
-             {/* <CopyToClipboard value={{url}} onCopy={onCopyText}> */}
-                 {/* <span>{isCopied ? "Copied!" : <MdContentCopy/>}</span> */}
-                 {/* </CopyToClipboard> */}
+             {/* ![]({url}) */}
+             
+                 <CopyToClipboard text={"![](" +url+ ")"} onCopy={onCopyText}>
+                 <span>{isCopied ? <h3>"Copied!"</h3> : <IconContext.Provider value={{style:{fontSize:'50px',color: 'Purple'}}}><MdContentCopy/>
+                 Copy to clipboard
+                 </IconContext.Provider>}</span>
+                 </CopyToClipboard>
              
              
             </div>
